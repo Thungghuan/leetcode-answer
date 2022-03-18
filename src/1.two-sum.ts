a1a8d408e60ab50b16fb4fb245faddf56566b2f4
Thungghuan <1092887426@qq.com>
2022-03-18 09:45:55 +0800

    fix: typo

--- src/1.two-sum.ts
+++ src/1.two-sum.ts
@@ -43,7 +43,7 @@ function twoSum(nums: number[], target: number): number[] {
 if (import.meta.vitest) {
   const { expect, it } = import.meta.vitest
 
-  it('tow-sum', () => {
+  it('two-sum', () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
     expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
     expect(twoSum([3, 3], 6)).toEqual([0, 1])
