77d9eeb99a9f1947d057669fb3ad3b3ee8bd2c37
Thungghuan <1092887426@qq.com>
2022-03-24 16:18:45 +0800

    fix: createTree

--- src/types.ts
+++ src/types.ts
@@ -39,21 +39,140 @@ export class TreeNode {
 }
 
 export function createTree(nodes: (number | null)[]): TreeNode | null {
-  // case: `[]`
-  if (nodes.length === 0) return null
+  if (nodes.length === 0 || nodes[0] === null) return null
 
-  const first = nodes.shift()
+  const root = new TreeNode(nodes[0])
+  const nodeQueue: TreeNode[] = [root]
 
-  // case: `[null]`
-  if (!first) return null
+  let insert = 1
 
-  // case: `[number]`
-  // length become zero after `shift`
-  if (nodes.length === 0) return new TreeNode(first)
+  while (insert < nodes.length) {
+    const node = nodeQueue.shift()!
 
-  const tree = new TreeNode(first)
-  tree.left = createTree(nodes)
-  tree.right = createTree(nodes)
+    const left = nodes[insert]
+    if (left) {
+      node.left = new TreeNode(left)
+      nodeQueue.push(node.left)
+    } else {
+      node.left = null
+    }
 
-  return tree
+    insert++
+    if (insert === nodes.length) break
+
+    const right = nodes[insert]
+    if (right) {
+      node.right = new TreeNode(right)
+      nodeQueue.push(node.right)
+    } else {
+      node.right = null
+    }
+    insert++
+  }
+
+  return root
+}
+
+if (import.meta.vitest) {
+  const { expect, it } = import.meta.vitest
+
+  it('create-tree', () => {
+    expect(createTree([1, 2])).toMatchInlineSnapshot(`
+      TreeNode {
+        "left": TreeNode {
+          "left": null,
+          "right": null,
+          "val": 2,
+        },
+        "right": null,
+        "val": 1,
+      }
+    `)
+
+    expect(createTree([1, null, 2])).toMatchInlineSnapshot(`
+      TreeNode {
+        "left": null,
+        "right": TreeNode {
+          "left": null,
+          "right": null,
+          "val": 2,
+        },
+        "val": 1,
+      }
+    `)
+
+    expect(createTree([1, null, 2, 3])).toMatchInlineSnapshot(`
+      TreeNode {
+        "left": null,
+        "right": TreeNode {
+          "left": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 3,
+          },
+          "right": null,
+          "val": 2,
+        },
+        "val": 1,
+      }
+    `)
+
+    expect(createTree([1, 2, 2, 3, 4, 4, 3])).toMatchInlineSnapshot(`
+      TreeNode {
+        "left": TreeNode {
+          "left": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 3,
+          },
+          "right": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 4,
+          },
+          "val": 2,
+        },
+        "right": TreeNode {
+          "left": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 4,
+          },
+          "right": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 3,
+          },
+          "val": 2,
+        },
+        "val": 1,
+      }
+    `)
+
+    expect(createTree([1, 2, 2, null, 3, null, 3])).toMatchInlineSnapshot(
+      `
+      TreeNode {
+        "left": TreeNode {
+          "left": null,
+          "right": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 3,
+          },
+          "val": 2,
+        },
+        "right": TreeNode {
+          "left": null,
+          "right": TreeNode {
+            "left": null,
+            "right": null,
+            "val": 3,
+          },
+          "val": 2,
+        },
+        "val": 1,
+      }
+    `
+    )
+  })
 }
