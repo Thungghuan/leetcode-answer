27602432c972f464c34649b3882a0acce4654c3e
Thungghuan <1092887426@qq.com>
2022-05-10 23:13:55 +0800

    26: accepted

--- src/26.remove-duplicates-from-sorted-array.ts
+++ src/26.remove-duplicates-from-sorted-array.ts
@@ -6,12 +6,39 @@
 
 // @lc code=start
 function removeDuplicates(nums: number[]): number {
-  nums.splice(0, nums.length, ...new Set(nums))
+  // nums.splice(0, nums.length, ...new Set(nums))
 
-  return nums.length
+  // return nums.length
+
+  let slow = 0
+  let fast = 0
+  const len = nums.length
+
+  while (fast < len) {
+    while (fast !== len - 1 && nums[fast] === nums[fast + 1]) {
+      fast++
+    }
+    nums[slow] = nums[fast]
+
+    slow++
+    fast++
+  }
+
+  return slow
 }
 // @lc code=end
 
-const testArray = [1, 1, 2]
-console.log(removeDuplicates(testArray))
-console.log(testArray)
+// const testArray = [1, 1, 2]
+// console.log(removeDuplicates(testArray))
+// console.log(testArray)
+
+if (import.meta.vitest) {
+  const { expect, it } = import.meta.vitest
+
+  it('26.remove-duplicates-from-sorted-array', () => {
+    expect(removeDuplicates([1, 1, 2])).toMatchInlineSnapshot('2')
+    expect(
+      removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+    ).toMatchInlineSnapshot('5')
+  })
+}
